1c9a7ecd2597439ffc36df3c70558210ad6725c5
Supposeduck30 <173493181+Supposeduck30@users.noreply.github.com>
2026-03-22 04:58:35 +0000

    Implement kinematics package and refactor geometry for high performance
    - Implemented standard kinematics package (ChassisSpeeds, SwerveModuleState, Kinematics).
    - Added optimized Swerve, Mecanum, and Tank kinematics implementations.
    - Refactored entire library to use unified math.util.Pose and Vector classes.
    - Removed legacy Pose2d and Vector2d classes.
    - Optimized hot loops in HolonomicTrajectoryFollower and drivetrains to minimize object instantiation.
    - Resolved conflicts with master by updating new localizer implementations to the refactored geometry API.
    - Updated documentation and verified with unit tests.

---
+++ apexpathing/src/main/java/com/apexpathing/localization/Localizer.kt
@@ -0,0 +1,43 @@
+package com.apexpathing.localization
+
+
+import com.apexpathing.util.math.Vector
+import com.apexpathing.util.math.Pose
+import com.qualcomm.robotcore.hardware.HardwareMap
+
+/**
+ *@author Topher Fontana (@moosecoding)
+ *@date 3/19/26
+ */
+
+
+
+abstract class LocalizerBase() {
+    // Add the @Pathing annotation stuff here
+
+    // Stores the last position so that way we can calculate velocity
+    var lastPosition: Pose = Pose(0.0,0.0,0.0)
+    // Stores the last velocity so that way we can calculate acceleration
+    var lastVelocity: Vector = Vector(0.0,0.0)
+    var currentPosition: Pose = Pose(0.0,0.0,0.0)
+    var currentVelocity: Vector = Vector(0.0,0.0)
+    var currentAcceleration: Vector = Vector(0.0,0.0)
+
+
+
+    /**
+     * This initializes the localizer's hardware map or whatever else needed
+     */
+    abstract fun initLocalizer(hardwareMap: HardwareMap)
+
+
+    /**
+     * This is the update class called when your localizer is attached using the @Pathing annotation
+     */
+    abstract fun update()
+
+    /**
+     *@param A [Pose] to set the location of the localizer to
+     */
+    abstract fun setPose(pose: Pose)
+}
